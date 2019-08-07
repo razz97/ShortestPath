11e734e15c6be25ed9a35dbcb087828d6725a2df
alex <5alexbouoliete@gmail.com>
2019-08-08 00:03:07 +0100

    forgot to save restcontroller

--- src/main/java/net/ddns/alexraspberry/trains/beans/rest/RESTController.java
+++ src/main/java/net/ddns/alexraspberry/trains/beans/rest/RESTController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -30,7 +31,7 @@ public class RESTController {
 	
 	@GET
 	@Path("/route/{query}")
-	public Result getResult() {
+	public Result getResult(@PathParam("query") String query) {
 		
 		
 		return null;
